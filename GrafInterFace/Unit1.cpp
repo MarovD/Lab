617ba034e204d614e3d0420df098984adde7cfd2
Croc <rhjrjlbkmxb@gmail.com>
2022-03-23 16:42:10 +0500

    Начало работы

---
+++ GrafInterFace/Unit1.cpp
@@ -0,0 +1,31 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+#include "Unit1.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TForm1 *Form1;
+HINSTANCE MyDLL =LoadLibraryW(L"Project1.dll");
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+	PMyGroup MyGroup;
+	MyGroup =(PMyGroup)GetProcAddress(MyDLL,"MyGroup");
+	Label1->Caption=MyGroup();
+}
+//---------------------------------------------------------------------------
+void __fastcall TForm1::Button2Click(TObject *Sender)
+{
+
+	PMyName MyName;
+	MyName =(PMyName)GetProcAddress(MyDLL,"MyName");
+	Label1->Caption=MyName();
+}
+//---------------------------------------------------------------------------
